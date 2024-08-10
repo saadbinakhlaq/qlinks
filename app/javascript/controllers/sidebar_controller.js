50dae9fe09a8cbcf0ace42aa6d9a672b2934ae1c
Saad Bin Akhlaq <saad.18@gmail.com>
2024-08-10 08:28:29 +0200

    Remove hello

--- app/javascript/controllers/sidebar_controller.js
+++ app/javascript/controllers/sidebar_controller.js
@@ -1,13 +1,18 @@
 import { Controller } from "@hotwired/stimulus"
+import { useClickOutside } from "stimulus-use";
 
 export default class extends Controller {
   static targets = [ "profileButton" ]
 
   connect() {
+    useClickOutside(this)
+  }
+
+  clickOutside() {
+    this.profileButtonTarget.classList.add('hidden')
   }
 
   toggle() {
-    console.log("hi")
     if (this.profileButtonTarget.classList.contains('hidden')) {
       this.profileButtonTarget.classList.remove('hidden')
     } else {
